e88f1c8524ffd9b95a48e892478e382f75964c87
Zhou Fang <zhoufang928@gmail.com>
2018-05-21 11:25:23 +0800

    Reduce output to something more accessible to a user.

--- movie-recommender-r/demo.R
+++ movie-recommender-r/demo.R
@@ -22,9 +22,14 @@ dfu <- read.csv(file.path(ms_dir, "user10.csv"), header = TRUE)
 names(dfu) <- c("user", "item",  "rating", "time", "title", "genres")
 dfu$time <- as.POSIXct(as.numeric(as.character(dfu$time)), origin="1970-01-01", tz="GMT")
 
+# Number of users.
+
+nid <- dfu$user %>% unique() %>% length()
+
 # prediction per user (userId)
 
-recu <- user_predict(loc_count, userdata=dfu, n=5)
+nrec <- 2
+recu <- user_predict(loc_count, userdata=dfu, n=nrec)
 
 # evaluation
 
@@ -33,60 +38,46 @@ metrics <- user_pred_metrics(recu, dfu)
 # more interpretable output
 
 recu2 <- NULL
-recu2$user <- rep(as.numeric(recu$user), each=5)
-recu2$item <- as.vector(t(as.matrix(recu[, 2:6])))
+recu2$user <- rep(as.numeric(recu$user), each=nrec)
+recu2$item <- as.vector(t(as.matrix(recu[, 2:(nrec+1)])))
 recu2 <- as.data.frame(recu2)
 
 ms_map <- read.csv(file.path(ms_dir, "map.csv"), header = TRUE)
 mapu <- join(ms_map, recu2, type="right")
 recu2$title <- mapu$title
 
-cat("\n\n=====================
-    \nUser 1
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==1])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==1])), collapse="\n * "), 
-    "\n\n=====================
-    \nUser 2 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==2])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==2])), collapse="\n * "), 
-    "\n\n=====================
-    \nUser 3 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==3])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==3])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 4 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==4])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==4])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 5 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==5])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==5])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 6 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==6])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==6])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 7 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==7])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==7])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 8 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==8])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==8])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 9 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==9])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==9])), collapse="\n * "),
-    "\n\n=====================
-    \nUser 10 
-    \nWatched:\n\n *", paste(as.character(as.character(dfu$title[dfu$user==10])), collapse="\n * "), 
-    "\n\nModel Recommends:\n\n *", paste(as.character(as.character(recu2$title[recu2$user==10])), collapse="\n * "),
-    "\n\n=====================
-    \nOverall Model Performance Evaluation",
-    "\n\n=====================
-    \nPrecision =", mean(metrics$prec), "The precision is the proportion of top 5 recommendations were actually suitable for the user",
-    "\nRecall =", mean(metrics$rec), "The recall is the proportion of good recommendations that appear in top 5 recommendations",
-    "\n\n=====================\n\n
-    ")
+nuser <- 2
+cat("\nThe model is applied to", nid, "users to suggest their next movies to watch.")
+cat("\nHere we show a random", nuser, "users, listing watched movies and recommendation.\n\n")
+
+for (i in sample(nid, nuser))
+{
+  cat("========\n")
+  cat("User", sprintf("%03d", i))
+  cat("\n========\n\n")
+
+  wsmpl <- 5
+  cat("Watched\n  * ")
+  watched <- dfu$title[dfu$user==i] %>% sort()
+  if (wsmpl <= length(watched))
+    smpl <- sample(length(watched), wsmpl) %>% sort()
+  else
+    smpl <- 1:length(watched)
+  as.character(watched[smpl]) %>% paste(collapse="\n  * ") %>% cat()
+  cat("\n  * ...", length(watched)-wsmpl, "more ...")
+
+  cat("\n\nRecommend\n  * ")
+  recommend <- recu2$title[recu2$user==i]
+  as.character(recommend[1:nrec]) %>% paste(collapse="\n  * ") %>% cat()
+  cat("\n\n")
+}
+
+cat("====================================\n")
+cat("Overall Model Performance Evaluation")
+cat("\n====================================\n")
+
+cat("\n UNDER DEVELOPMENT\n\n")
+#cat("\nPrecision =", mean(metrics$prec), "Proportion of top 5 recommendations suitable for user.",
+#    "\nRecall    =", mean(metrics$rec),  "Proportion of good recommendations in top 5.\n\n")
 
 
