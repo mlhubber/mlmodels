9796968f7c0205cb70afc11972788ed0a6554c1c
Zhou Fang <zhoufang928@gmail.com>
2018-06-30 23:58:57 +0800

    enable the cmd score to run shiny app

--- dogs-cats-r/score.R
+++ dogs-cats-r/score.R
@@ -1,50 +1,98 @@
-# Load model, predict on a small dataset.
+# Load model, predict on a selected image, show on shiny app
 
 # load library
 
 library(keras)
 library(magrittr)
 library(dplyr)
+library(shiny)
+library(shinyjs)
+library(V8)
 
-# load model
+jsToggleFS <- 'shinyjs.toggleFullScreen = function() {
+    var element = document.documentElement,
+      enterFS = element.requestFullscreen || element.msRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen,
+      exitFS = document.exitFullscreen || document.msExitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen;
+    if (!document.fullscreenElement && !document.msFullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+      enterFS.call(element);
+    } else {
+      exitFS.call(document);
+    }
+  }'
 
-model <-
-load_model_hdf5(filepath = "dogs-cats-vgg16-model.hdf5", 
-                custom_objects = NULL, 
-                compile = TRUE)
+# define UI
 
-# file to score
-
-args <- commandArgs(trailingOnly = TRUE)
-fname <- args[1]
-
-# get score data
-
-base_dir <- "~/.mlhub/dogs-cats-r/data/cats_and_dogs_small"
-test_dir <- file.path(base_dir, fname)
-test_datagen <- image_data_generator(rescale = 1/255)
-
-cat("====================\nPredict Image Classes\n====================\n\n")
-
-test_generator <- flow_images_from_directory(
-  test_dir,
-  test_datagen,
-  target_size = c(150, 150),
-  batch_size = 20,
-  class_mode = "binary"
+ui <- fluidPage(
+  useShinyjs(),
+  extendShinyjs(text = jsToggleFS),
+  
+  titlePanel("Dogs vs Cats Image Classification"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("img", "Choose image file",
+                accept=c("image/jpeg", "image/png", "image/bmp"))
+    ),
+    mainPanel(
+      HTML('<output id="list"></output>'),
+      tags$hr(),
+      h3(textOutput("pred"))
+    )
+  )
 )
 
-pred_score <- model %>% predict_generator(test_generator, steps = 50)
-pred_class <- ifelse(pred_score > 0.5, 1, 0)
+# define server function
 
-img_names <- c(list.files(path = file.path(test_dir, "cats")), 
-               list.files(path = file.path(test_dir, "dogs")))
+server <- function(input, output){
+  
+  model <- 
+    load_model_hdf5(filepath = "dogs-cats-vgg16-model.hdf5", 
+                    custom_objects = NULL, 
+                    compile = TRUE)
+  model.weights <-
+    load_model_weights_hdf5(model,
+                            filepath = "dogs-cats-vgg16-model-weights.hdf5")
+  
+  model %>% compile(
+    loss = "binary_crossentropy",
+    optimizer = optimizer_rmsprop(lr = 2e-5),
+    metrics = c("accuracy"))
+  
+  showImgJs <- paste(readLines("showImg.js"), collapse="\n")
+  shinyjs::runjs(showImgJs)
+  
+  output$pred <- renderText({
+    if(is.null(input$img))
+      return("")
+    img_name <- input$img$name
+    img_path <- input$img$datapath
+    img <- paste0(as.character(readBin(img_path, raw(), file.size(img_path))), collapse="")
+    
+    img_dir <- dirname(dirname(img_path))
+    img_datagen <- image_data_generator(rescale = 1/255)
+    
+    img_generator <- flow_images_from_directory(
+      img_dir,
+      img_datagen,
+      target_size = c(150, 150),
+      batch_size = 20,
+      class_mode = "binary"
+    )
+    
+    pred_score <- model %>% 
+      predict_generator(img_generator, steps = 1)
+    pred_class <- ifelse(pred_score > 0.5, "dogs", "cats")
+    
+    out <- paste("Predicted Class =", as.character(pred_class))
+    out
+  })
+  
+}
 
-img_names %>%
-  as.data.frame() %>% 
-  cbind(Predicted_Class = pred_class) %>%
-  cbind(Predicted_Score = pred_score) %>%
-  set_names(c("Image", "Predicted_Class", "Predicted_Score")) %>%
-  head(n = 20) %T>%
-  print() ->
-  ev
+# define shiny app
+
+app <- shinyApp(ui = ui, server = server)
+
+# run shiny app
+
+runApp(app, launch.browser = TRUE)
