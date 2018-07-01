8888397567a7ab1ab3654d614ada914cb8f7b512
Graham Williams <Graham.Williams@togaware.com>
2018-07-01 11:57:08 +0800

    Initial import - add setup.sh to set up the model.

---
+++ dogs-cats-r/setup.sh
@@ -0,0 +1,5 @@
+# Need a script here to create the prebuilt models. They are not put
+# into github as they are large and also they are generated. This is
+# not something for the user but for the model builder.
+#
+# See resnet152 for an example.
