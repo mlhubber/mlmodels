a687a6a30d0c2158197076a56dd197c083cf580b
Graham Williams <Graham.Williams@togaware.com>
2018-07-01 11:49:08 +0800

    Initial import - cleaning up to match current template.

---
+++ clothes-recommender-py/setup.sh
@@ -0,0 +1,8 @@
+# We need to set up proc/fashionTexture to contain the pre-trained ML
+# models:
+#
+#  cntk.model;
+#  svm.np
+#
+# This setup script should take care of it. We do not put them into
+# github (too large). See the resnet152 model for an exapmle.
