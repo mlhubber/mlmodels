656ace8b14a73a74eea21390a823398f882c5498
Graham Williams <Graham.Williams@togaware.com>
2018-07-01 16:44:06 +0800

    Add commentary about dogs-cats data source.

--- dogs-cats-r/setup.sh
+++ dogs-cats-r/setup.sh
@@ -3,3 +3,11 @@
 # not something for the user but for the model builder.
 #
 # See resnet152 for an example.
+#
+# Data seems to come from Kaggle. Is thre another source not requiring
+# a login? Model author needs to confirm licensing limitations before
+# the data can be distributed. Then in this script include the
+# download instructions.
+#
+# The prebuilt model files come from the R script train.R.
+
