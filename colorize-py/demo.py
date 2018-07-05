5e69262f7dffa86dd95091f309d1cc16312eff9a
Zhou Fang <zhoufang928@gmail.com>
2018-07-05 16:48:34 +0800

    upload colorize-py

---
+++ colorize-py/demo.py
@@ -0,0 +1,28 @@
+# Import the necessary packages.
+
+import os
+import cv2 as cv
+import glob
+
+from utils import predict
+
+if __name__ == '__main__':
+    image_folder = 'images'
+
+    images = glob.glob("images/*_bw.png")
+    images.sort()
+
+    cwd = os.getcwd()
+    for image in images:
+        print("Colorize " + os.path.join(cwd, image))
+        gray = cv.imread(image, 0)
+        out = predict(gray)
+        cv.imwrite(image.replace("bw", "color"), out)
+
+msg = """
+The individual colorized images can be displayed:
+
+ $ ml display colorize-py
+"""
+print(msg)
+
