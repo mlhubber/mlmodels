4de4f4c7ff80e6a59b6c4012277e6b559bd12248
Graham Williams <graham.williams@togaware.com>
2018-07-05 16:01:36 +0800

    Finish tuning.

--- colorize-py/demo.py
+++ colorize-py/demo.py
@@ -1,14 +1,28 @@
-# import the necessary packages
-import os
+# Import the necessary packages.
 
+import os
 import cv2 as cv
+import glob
 
 from utils import predict
 
 if __name__ == '__main__':
     image_folder = 'images'
-    image_name = os.path.join(image_folder, 'sample.png')
-    gray = cv.imread(image_name, 0)
-    out = predict(gray)
 
-    cv.imwrite('images/output.png', out)
+    images = glob.glob("images/*_bw.png")
+    images.sort()
+
+    cwd = os.getcwd()
+    for image in images:
+        print("Colorize " + os.path.join(cwd, image))
+        gray = cv.imread(image, 0)
+        out = predict(gray)
+        cv.imwrite(image.replace("bw", "color"), out)
+
+msg = """
+The individual colorized images can be displayed:
+
+ $ ml display colorize-py
+"""
+print(msg)
+
