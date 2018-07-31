5b6296b06846f21c98a9a6bf235174d173ee1907
Zhou Fang <zhoufang928@gmail.com>
2018-07-31 15:25:29 +0800

    update configure files

--- dogs-cats-r/configure.R
+++ dogs-cats-r/configure.R
@@ -1,8 +1,11 @@
-packages <- c("magrittr", "dplyr", "caret", "e1071", "shiny", "shinyjs", "V8")
+packages <- c("magrittr", "dplyr", "caret", "e1071", "shiny", "shinyjs", "V8", "devtools", "httr")
 install <- packages[!(packages %in% installed.packages()[, "Package"])]
 dir.create(Sys.getenv("R_LIBS_USER"), showWarnings=FALSE, recursive=TRUE)
 if (length(install)) install.packages(install, lib=Sys.getenv("R_LIBS_USER"))
 
-dev_packages <- c("tfruns", "reticulate", "keras")
+library(httr)
+set_config(config(ssl_verifypeer=0L))
+
+dev_packages <- c("rstudio/tfruns", "rstudio/reticulate", "rstudio/keras")
 dev_install <- dev_packages[!(dev_packages %in% installed.packages()[, "Package"])]
 if (length(dev_install)) devtools::install_github(dev_install, lib=Sys.getenv("R_LIBS_USER"))
--- dogs-cats-r/configure.sh
+++ dogs-cats-r/configure.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 sudo apt-get install libv8-dev
+sudo apt-get install python-pip
+sudo pip install --upgrade pip
 
+conda create --name r-tensorflow python=2.7
+source activate r-tensorflow
+pip install --ignore-installed --upgrade tensorflow
+conda install -c conda-forge keras
+
+pip install Image
+
+Rscript configure.R
