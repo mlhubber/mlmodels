76e805b881b39d19de802ecd0609a60e0eba3023
Zhou Fang <zhoufang928@gmail.com>
2018-07-31 15:29:50 +0800

    update setup.sh

--- dogs-cats-r/setup.sh
+++ dogs-cats-r/setup.sh
@@ -4,10 +4,13 @@
 #
 # See resnet152 for an example.
 #
-# Data seems to come from Kaggle. Is thre another source not requiring
+# Data seems to come from Kaggle. Is there another source not requiring
 # a login? Model author needs to confirm licensing limitations before
 # the data can be distributed. Then in this script include the
 # download instructions.
+# 
+# Kaggle dogs vs cats data set: https://www.kaggle.com/c/dogs-vs-cats/data
+# Microsoft Research: https://www.microsoft.com/en-us/download/details.aspx?id=54765
 #
 # The prebuilt model files come from the R script train.R.
 
