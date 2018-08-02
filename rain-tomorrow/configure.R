45ae2dac17d9ed7ce20bb26f80f2622653ddf3c1
Graham Williams <graham.williams@togaware.com>
2018-08-02 13:35:14 +0800

    Update commentary.

--- rain-tomorrow/configure.R
+++ rain-tomorrow/configure.R
@@ -1,4 +1,20 @@
+# Ubuntu system dependencies for the pre-built model. We choose to
+# install user local packages rather than OS supplied packages to
+# avoid the need for sys admin access from within mlhub. Nontheless we
+# assume R is operating system installed.
+
+# Identify the required packages.
+
 packages <- c("rpart", "magrittr")
+
+# Determine which need to be installed.
+
 install  <- packages[!(packages %in% installed.packages()[,"Package"])]
+
+# Ensure the user's local R library exists.
+
 dir.create(Sys.getenv("R_LIBS_USER"), showWarnings=FALSE, recursive=TRUE)
+
+# Install the packages local to the user.
+
 if (length(install)) install.packages(install, lib=Sys.getenv("R_LIBS_USER"))
