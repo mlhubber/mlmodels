4235acc83e91c197faf4fc55b4f9c42d9fa15c37
Graham Williams <graham.williams@togaware.com>
2018-08-04 16:46:02 +0800

    Update comments.

--- rain-tomorrow/display.R
+++ rain-tomorrow/display.R
@@ -1,7 +1,16 @@
+# The display command aims to provide a visualisation of the model in
+# some form. This will differen from model to model.
+
 # Display the sample decision tree model.
 
 library(rpart)
+
+# We include a simple rattle script so as not to require loading all
+# of rattle.
+
 source("rattle.R")
+
+# If a display is available then make use of it.
 
 if (Sys.getenv("DISPLAY") != "")
 {
@@ -21,7 +30,7 @@ if (Sys.getenv("DISPLAY") != "")
   cat("Graphic display not available.\n")
 }
 
-# Suggest next step.
+# It is always polite to suggest the next step for the user.
 
 cat("\nYou may like to predict if it will rain tomorrow:\n",
     "\n  $ ml score rain-tomorrow\n\n")
