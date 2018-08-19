59908be52dea814b09d93b4d8ef818c36dc043d4
Graham Williams <Graham.Williams@togaware.com>
2018-08-19 11:41:32 +0800

    Be more informative on what is configured.

--- barchart/configure.R
+++ barchart/configure.R
@@ -1,4 +1,13 @@
-packages <- c("rpart", "magrittr", "RColorBrewer", "rattle.data")
+packages <- c("rpart", "magrittr", "RColorBrewer")
 install  <- packages[!(packages %in% installed.packages()[,"Package"])]
-dir.create(Sys.getenv("R_LIBS_USER"), showWarnings=FALSE, recursive=TRUE)
-if (length(install)) install.packages(install, lib=Sys.getenv("R_LIBS_USER"))
+lib <- Sys.getenv("R_LIBS_USER")
+dir.create(lib, showWarnings=FALSE, recursive=TRUE)
+if (length(install))
+{
+  cat(sprintf("\nInstalling '%s' into '%s'...", paste(install, collapse="', '"), lib))
+  install.packages(install, lib=lib)
+} else
+{
+  cat("\nNo additional R packages need to be installed.")
+}
+cat("\n")
