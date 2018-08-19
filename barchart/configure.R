c1d5d8b16c7fffb8497b575d5fe6fbc9fe763133
Graham Williams <Graham.Williams@togaware.com>
2018-08-19 21:01:04 +0800

    Fully comment configure.R for barchart consistent with
    rain-tomorrow. Aim for a generic version implemented in mlhub command.

--- barchart/configure.R
+++ barchart/configure.R
@@ -1,7 +1,26 @@
+# Ubuntu system dependencies for the pre-built model. We choose to
+# install user local packages rather than OS supplied packages to
+# avoid the need for sys admin access from within mlhub. R is
+# often operating system installed.
+
+# Identify the required packages.
+
 packages <- c("rpart", "magrittr", "RColorBrewer")
+
+# Determine which need to be installed.
+
 install  <- packages[!(packages %in% installed.packages()[,"Package"])]
+
+# Identify where they will be installed - the user's local R library.
+
 lib <- Sys.getenv("R_LIBS_USER")
+
+# Ensure the user's local R library exists.
+
 dir.create(lib, showWarnings=FALSE, recursive=TRUE)
+
+# Install the packages into the local R library.
+
 if (length(install))
 {
   cat(sprintf("\nInstalling '%s' into '%s'...", paste(install, collapse="', '"), lib))
