81d06458ebfa0bc1af3bff9f891caab2152a1063
Zhou Fang <zhoufang928@gmail.com>
2018-08-21 15:17:51 +0800

    upload dog vs cat image data from PEXELS

--- dogs-cats-r/setup.sh
+++ dogs-cats-r/setup.sh
@@ -4,13 +4,18 @@
 #
 # See resnet152 for an example.
 #
-# Data seems to come from Kaggle. Is there another source not requiring
-# a login? Model author needs to confirm licensing limitations before
-# the data can be distributed. Then in this script include the
+# Data used in the Rstudio tutorial come from Kaggle. Because of the licensing limitations,
+# the data can not be distributed. Then in this script include the
 # download instructions.
 # 
 # Kaggle dogs vs cats data set: https://www.kaggle.com/c/dogs-vs-cats/data
 # Microsoft Research: https://www.microsoft.com/en-us/download/details.aspx?id=54765
+#
+# We replace the test dataset with a smaller-sized public dog and cat image data, 
+# which can be distributed and used for personal and commercial purpose.
+#
+# https://www.pexels.com/search/cat/
+# https://www.pexels.com/search/dog/
 #
 # The prebuilt model files come from the R script train.R.
 
