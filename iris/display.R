f760c2bc76270a8ff91fc86c2f247eec13b07647
Graham Williams <Graham.Williams@togaware.com>
2018-11-04 08:57:45 +0800

    Rewrite - cleaner and test DISPLAY.

--- iris/display.R
+++ iris/display.R
@@ -1,13 +1,15 @@
+suppressMessages(
+{
 library(rattle)
+})
 
 load("iris_model.RData")
 
-results <- "results"
-fname <- file.path(results, "dtree.pdf")
-  
-if (!dir.exists(results)) dir.create(results)
-
-pdf(fname)
-fancyRpartPlot(m$finalModel, sub="")
-tmp <- dev.off() # Assign to avoid output.
-system(paste("display -background white -flatten", fname))
+if (Sys.getenv("DISPLAY") != "")
+{
+  fname <- "iris_rpart_model.pdf"
+  pdf(fname)
+  fancyRpartPlot(m$finalModel, sub="")
+  invisible(dev.off())
+  system(paste("evince --preview", fname))
+}
