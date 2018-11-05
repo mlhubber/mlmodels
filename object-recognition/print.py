d5a4008ed2a5b495ded949f830957a5b41cd226b
Graham Williams <graham.williams@togaware.com>
2018-11-05 08:46:46 +0800

    Move object-recognition into separate repository.

--- object-recognition/print.py
+++
@@ -1,9 +0,0 @@
-print("Loading the required Python modules for the ResNet152 model.")
-import tensorflow as tf
-import keras
-from objreg_utils import ResNet152
-
-print("\nLoading the ResNet model.")
-model = ResNet152(weights='imagenet')
-
-model.summary()
