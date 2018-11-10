e6cebcddf7a18ac45c7b20b021c881764a514314
Graham Williams <Graham.Williams@togaware.com>
2018-11-11 05:52:56 +0800

    Move pets and clothes into own repo.

--- clothes-recommender/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Ubuntu: Tested on Azure 16.04 DLVM
-
-sudo apt-get install -y wajig
-wajig update
-wajig distupgrade -y
-wajig install -y python-opencv python-requests python-matplotlib python-scipy python-sklearn eog
