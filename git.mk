3fe3483d64ea1fc8669075ef1fbae8ea5ae8d85f
Graham Williams <git@togaware.com>
2018-11-22 11:56:44 +0800

    Add commentary using git

--- git.mk
+++ git.mk
@@ -26,6 +26,44 @@ GIT Version Control:
 endef
 export GIT_HELP
 
+########################################################################
+#
+# Using git - see https://gist.github.com/Chaser324/ce0505fbed06b947d962
+#
+# On github click Fork on the original repository
+# Clone your fork to your local machine
+#   $ git clone git@github.com:gjwgit/pname.git
+#   $ cd pname
+# Add 'upstream' repo to list of remotes
+#   $ git remote add upstream https://github.com/UPSTREAM-USER/ORIGINAL-PROJECT.git
+# Verify the new remote named 'upstream'
+#   $ git remote -v
+# Fetch from upstream remote
+#   $ git fetch upstream
+# View all branches, including those from upstream
+#   $ git branch -va
+# Checkout your master branch and merge upstream
+#   $ git checkout master
+#   $ git merge upstream/master
+# Checkout the master branch - you want your new branch to come from master
+#   $ git checkout master
+# Create a new branch named newfeature (give your branch its own simple informative name)
+#   $ git branch newfeature
+# Switch to your new branch
+#   $ git checkout newfeature
+# DO YOUR WORK
+# Fetch upstream master and merge with your repo's master branch
+#   $ git fetch upstream
+#   $ git checkout master
+#   $ git merge upstream/master
+# If there were any new commits, rebase your development branch
+#   $ git checkout newfeature
+#   $ git rebase master
+# Rebase all commits on your development branch
+#   $ git checkout 
+#   $ git rebase -i master
+# On github change to branch and click Pull Request
+
 help::
 	@echo "$$GIT_HELP"
 
